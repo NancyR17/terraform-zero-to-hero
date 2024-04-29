a6dbb289ecd20a41b715b05aff67033da49de032
NancyR17 <111511876+NancyR17@users.noreply.github.com>
2024-04-30 00:39:14 +0530

    Update main.tf_ with practical code

--- Day-3/modules/ec2_instance/main.tf
+++ Day-3/modules/ec2_instance/main.tf
@@ -2,8 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "naina-example" {
     ami = var.ami_value
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
+    tags = {
+    Name = "naina-example"
+  }
 }
